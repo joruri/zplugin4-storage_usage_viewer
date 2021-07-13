063a5a6ba9dcf7a7527744af7e633d86dfb7bce8
mmukai <mukai@sitebridge.co.jp>
2021-07-13 09:52:56 +0900

    Fix misc

--- app/controllers/zplugin/storage/usage/viewer/admin/usages_controller.rb
+++ app/controllers/zplugin/storage/usage/viewer/admin/usages_controller.rb
@@ -1,18 +1,22 @@
 class Zplugin::Storage::Usage::Viewer::Admin::UsagesController < Zplugin::Storage::Usage::Viewer::Admin::BaseController
 
   def pre_dispatch
-    @policy = authorize(policy: Zplugin::Storage::Usage::Viewer::UsagePolicy)
+    @policy = authorize(Zplugin::Storage::Usage::Viewer::UsagePolicy)
   end
 
   def index
+    usages = Zplugin::Storage::Usage::Viewer::Usage.all
     @criteria = criteria
-    @items = Zplugin::Storage::Usage::Viewer::Usage.finder(@criteria).order(size: :desc)
-    @items = @items.paginate(page: params[:page], per_page: params[:limit])
+    items = Zplugin::Storage::Usage::Viewer::UsagesFinder.call(usages, @criteria)
+                                   .order(size: :desc, id: :desc)
 
     if params[:csv]
-      csv = Zplugin::Storage::Usage::Viewer::Export::UsagesInteractor.call(items: @items).result
-      return send_data platform_encode(csv), type: Rack::Mime.mime_type('.csv'), filename: "files_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv"
+      csv = Zplugin::Storage::Usage::Viewer::Export::UsagesInteractor.call(items: items).result
+      return send_data platform_encode(csv), type: Rack::Mime.mime_type('.csv'), filename: "storage_usages_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv"
     end
+
+    @items = items.paginate(page: params[:page], per_page: params[:limit])
+    _index @items
   end
 
   private
