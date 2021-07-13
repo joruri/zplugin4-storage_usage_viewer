46a3f155744f442554ad6d1116a61e5c58808d5a
mmukai <mukai@sitebridge.co.jp>
2021-07-13 14:06:00 +0900

    Fix misc

--- app/jobs/zplugin/storage/usage/viewer/check_job.rb
+++ app/jobs/zplugin/storage/usage/viewer/check_job.rb
@@ -2,6 +2,8 @@ class Zplugin::Storage::Usage::Viewer::CheckJob < ApplicationJob
   def perform
     require "open3"
 
+    st = DateTime.now
+
     Cms::Site.all.each do |site|
       command = "du -s #{site.root_path}"
       stdout, stderr, status = Open3.capture3(command)
@@ -12,5 +14,7 @@ class Zplugin::Storage::Usage::Viewer::CheckJob < ApplicationJob
       item.save
     end
 
+    at = Zplugin::Storage::Usage::Viewer::Usage.arel_table
+    Zplugin::Storage::Usage::Viewer::Usage.where(at[:updated_at].lt(st)).destroy_all
   end
 end
